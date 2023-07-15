2e6f4e74ce81e5ce9123ef889c62bff14a7c9896
Andrew Wong <andrew.j.wong@outlook.com>
2023-07-16 02:57:19 +1000

    Android lab

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# COMP6845 Template
+# COMP6845 Android Forensics Lab
+
+> https://featherbear.cc/UNSW-COMP6845-android - View sections individually
+
+---
+
+> https://featherbear.cc/UNSW-COMP6845-android - View all sections
--- content/pages.ts
+++ content/pages.ts
@@ -1,9 +1,21 @@
-import Welcome from "./sections/01-welcome.svx";
-import Next from "./sections/02-next.svx"
+import Resources from './sections/01-resources.svx'
+import ChainOfCustody from './sections/02-chain-of-custody.svx'
+import Hashsum from './sections/02.5-hashsum.svx'
+import SMS from './sections/03-sms.svx'
+import DatabaseAnalysis from './sections/04-database-analysis.svx'
+import DatabaseDeletedRecords from './sections/05-database-deleted-records.svx'
+import AutomatedAnalysis from './sections/06-automated-analysis.svx'
+import Questions from './sections/questions.svx'
 
 const pages = [
-  ["Welcome", Welcome],
-  ["Next", Next]
+  ["Resources", Resources],
+  ["Chain Of Custody", ChainOfCustody],
+  ["Getting Started", Hashsum],
+  ["SMS", SMS],
+  ["Database Analysis", DatabaseAnalysis],
+  ["Deleted Database Records", DatabaseDeletedRecords],
+  ["Automated Analysis", AutomatedAnalysis],
+  ["Questions", Questions],
 ] satisfies [string, any][];
 
 export default pages
